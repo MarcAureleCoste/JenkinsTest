5a2de0a039f68a39ce1c6557afad6522c0a73ffa
MarcAureleCoste <c.marc.aurele@outlook.com>
2019-04-16 15:21:44 +0200

    Update and rename mySupaFunc to mySupaFunc.groovy

---
+++ jenkins/mySupaFunc.groovy
@@ -0,0 +1,10 @@
+def mySupaFunc(String ecrURL, String imageName, String tag) {
+    def img="$ecrURL/$imageName:$tag"
+    def test="$ecrURL/$imageName:$GIT_COMMIT"
+    sh """
+        echo 'SUPA FUNCTION !'
+        ls -l
+        echo $img
+        echo $test
+    """
+}
